b4ad1a5f25dadea1c6eff1b29f4fb7a1c1459ced
aqiu <819110812@qq.com>
2020-03-30 10:27:00 +0800

    Add files via upload

--- 文本分类/TextCnn/model/TextCNN.py
+++ 文本分类/TextCnn/model/TextCNN.py
@@ -8,10 +8,10 @@ class TextCnn(nn.Module):
         super(TextCnn, self).__init__()
         self.args = args
         label_num = args.label_num
-        filter_num = args.filte_num
-        filter_size = [int(fsz) for fsz in args.filter_size(",")]
+        filter_num = args.filter_num
+        filter_size = [int(fsz) for fsz in args.filter_sizes.split(",")]
         vocab_size = args.vocab_size
-        embedding_dim = args = args.embedding_dim
+        embedding_dim = args.embedding_dim
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
         if args.static:  #是否加载预训练模型
             self.embedding = self.embedding.from_pretrained(args.vectors, freeze=not args.fine_tune)
@@ -30,7 +30,7 @@ class TextCnn(nn.Module):
         # 输入x的维度为(batch_size, max_len), max_len可以通过torchtext设置或自动获取为训练样本的最大=长度
         x = self.embedding(x) # 经过embedding,x的维度为(batch_size, max_len, embedding_dim)
         # 经过view函数x的维度变为(batch_size, input_chanel=1, w=max_len, h=embedding_dim)
-        x = x.view(x.size(0),1,x.size,self.args.embedding_dim)
+        x = x.view(x.size(0),1,x.size(1),self.args.embedding_dim)
         # 经过卷积运算,x中每个运算结果维度为(batch_size, out_chanel, w, h=1)
         '''
         F.relu和nn.Relu 从功能上来讲没有本质区别，F.relu是封装好的函数，nn.ReLu是一个层，依赖于nn.Module，F.relu在forward过程是不存在参数的
@@ -38,7 +38,7 @@ class TextCnn(nn.Module):
         x = [F.relu(conv(x)) for conv in self.convs]
 
         # 经过最大池化层,维度变为(batch_size, out_chanel, w=1, h=1)
-        x = [F.max_pool2d(input=x_item,kernal_size=(x_item.size(2),x_item.size(3))) for x_item in x]
+        x = [F.max_pool2d(input=x_item,kernel_size=(x_item.size(2),x_item.size(3))) for x_item in x]
         # 将不同卷积核运算结果维度（batch，out_chanel,w,h=1）展平为（batch, outchanel*w*h）
         x = [x_item.view(x_item.size(0),-1) for x_item in x]
 
